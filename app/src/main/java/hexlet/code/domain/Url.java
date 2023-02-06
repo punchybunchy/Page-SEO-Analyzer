d47614e6ab6bf1ab25abb11c0d3c9dbe01cf0ff4
tyoma <artyomlosev.work@gmail.com>
2023-02-06 22:52:17 +0600

    Update Url.java

--- app/src/main/java/hexlet/code/domain/Url.java
+++ app/src/main/java/hexlet/code/domain/Url.java
@@ -43,7 +43,7 @@ public class Url extends Model {
         this.name = name;
     }
 
-    public List<UrlCheck> getUrlChecks() {
+    public final List<UrlCheck> getUrlChecks() {
         return urlChecks;
     }
 
